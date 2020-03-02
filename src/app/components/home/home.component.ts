4922a431c2b14267e7bbd87d1b3eff2a5e777fde
mpietraszuk <GitHub@MarkZDesign.com>
2020-03-02 09:04:55 -0800

    added products to homepage and built some functionalility for add to cart that displays in console.log

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Interface/products.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  Products: Array<Product> = [
+    { Name: 'Cream', Price: 50, Desc: 'Album Cover', ProductPath: '../../../assets/img/Cream01-570x866.jpg' },
+    { Name: 'Jimi Hendrix', Price: 50, Desc: 'Jimi Hendrix in Copenhagen, Denmark 1970 music poster', ProductPath: '../../../assets/img/jimi-hendrix-copenhagen-1970-music-poster.jpg' },
+    { Name: 'Led Zepplin', Price: 50, Desc: 'Led Zepplin at Chicago Stadium, July 6, 1973', ProductPath: '../../../assets/img/LedZepplin1973.jpg' },
+    { Name: 'Pearl Jam', Price: 50, Desc: 'Pearl Jam in Berlin, Germany 2014', ProductPath: '../../../assets/img/PearlJam01.jpg' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addToCart(index) {
+    console.log("Added", this.Products[index]);
+  }
 }
